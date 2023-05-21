8b248b2a923c55600d794b491c1d85a5604fad17
kostrominoff <zootikchannel@gmail.com>
2023-05-21 19:08:44 +0500

    Fix button styles

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import Button from "@/components/ui/button";
+import Button, { ButtonStyles } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <Button>Hello world</Button>
-      <Button disabled onClick={() => console.log("test")}>Hello world</Button>
+      <Button disabled onClick={() => console.log("test")}>
+        Hello world
+      </Button>
+      <Link className={ButtonStyles("primary")} href={"/fff"}>
+        Hello
+      </Link>
     </div>
   );
 };
--- packages/client/src/components/ui/button/index.tsx
+++ packages/client/src/components/ui/button/index.tsx
@@ -5,8 +5,10 @@ import { AnimatePresence, MotionProps, motion } from "framer-motion";
 import { ButtonHTMLAttributes } from "react";
 import Icons from "../icons/index";
 
+type Variant = "primary" | "secondary" | "tertiary";
+
 type Props = {
-  variant?: "primary" | "secondary" | "tertiary";
+  variant?: Variant;
   loading?: boolean;
   disabled?: boolean;
   onlyIcon?: boolean;
@@ -42,6 +44,20 @@ const ButtonLoader = ({ primary = true, onlyIcon }: ButtonLoaderProps) => {
   );
 };
 
+const style = {
+  main: "inline-block px-4 text-base font-semibold rounded-lg transition py-[10px]",
+  primary: "text-white bg-indigo-600 hover:bg-indigo-700",
+  secondary: "bg-slate-100 text-slate-900 hover:bg-slate-200",
+  tertiary: "bg-transparent text-slate-600 hover:bg-slate-100",
+};
+
+export const ButtonStyles = (variant: Variant) =>
+  clsx(style.main, {
+    [style.primary]: variant === "primary",
+    [style.secondary]: variant === "secondary",
+    [style.tertiary]: variant === "tertiary",
+  });
+
 const Button = ({
   variant = "primary",
   loading,
@@ -52,17 +68,14 @@ const Button = ({
   ...props
 }: Props) => {
   return (
-    <motion.button
+    <button
       className={clsx(
-        "flex flex-row justify-center items-center px-4 text-base font-semibold rounded-lg transition py-[10px]",
-        className,
+        style.main,
+        "flex flex-row justify-center items-center",
         {
-          "text-white bg-indigo-600 hover:bg-indigo-700":
-            variant === "primary" && !loading && !disabled,
-          "bg-slate-100 text-slate-900 hover:bg-slate-200":
-            variant === "secondary" && !loading && !disabled,
-          "bg-transparent text-slate-600 hover:bg-slate-100":
-            variant === "tertiary" && !loading && !disabled,
+          [style.primary]: variant === "primary" && !loading && !disabled,
+          [style.secondary]: variant === "secondary" && !loading && !disabled,
+          [style.tertiary]: variant === "tertiary" && !loading && !disabled,
 
           "cursor-wait": loading,
           "text-white bg-indigo-400 hover:bg-indigo-400":
@@ -78,7 +91,8 @@ const Button = ({
             disabled && variant === "tertiary",
 
           "py-3 px-3": onlyIcon,
-        }
+        },
+        className
       )}
       disabled={disabled || loading}
       {...props}
@@ -98,7 +112,7 @@ const Button = ({
           <ButtonLoader onlyIcon={onlyIcon} primary={variant === "primary"} />
         )}
       </AnimatePresence>
-    </motion.button>
+    </button>
   );
 };
 
