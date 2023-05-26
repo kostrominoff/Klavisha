cfc6217523ce5692166f5449f9eb5223c128b106
kostrominoff <zootikchannel@gmail.com>
2023-05-26 14:38:39 +0500

    Users controller and service

--- packages/server/src/users/dto/create-user.dto.ts
+++ packages/server/src/users/dto/create-user.dto.ts
@@ -1,15 +1,40 @@
 import { ICreateUserDto, Roles } from '@klavisha/types';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto implements ICreateUserDto {
+  @ApiProperty({
+    description: 'Почта',
+    example: 'myemail@example.com',
+  })
   @IsEmail({}, { message: 'Проверьте почту!' })
   email: string;
+  @ApiProperty({
+    description: 'Пароль',
+    minLength: 8,
+  })
   @MinLength(8, { message: 'Пароль должен иметь не менее 8 символов!' })
   password: string;
+  @ApiProperty({
+    description: 'Роли пользователя',
+    enum: Roles,
+    isArray: true,
+    nullable: true,
+  })
   roles?: Roles[];
+  @ApiProperty({
+    description: 'Имя пользователя',
+  })
   @IsString({ message: 'Проверьте имя!' })
   firstname: string;
+  @ApiProperty({
+    description: 'Фамилия пользователя',
+  })
   @IsString({ message: 'Проверьте фамилию!' })
   secondname: string;
+  @ApiProperty({
+    description: 'Отчество пользователя',
+    nullable: true
+  })
   fathername?: string;
 }
--- packages/server/src/users/users.controller.ts
+++ packages/server/src/users/users.controller.ts
@@ -1,7 +1,96 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  // TODO: Auth guard
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Успешный ответ',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Пользователь не найден',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Пользователь не авторизован',
+  })
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  async getOne(@Param('id', ParseIntPipe) userId: number) {
+    return await this.usersService.findOneById(userId);
+  }
+
+  // TODO: Admin and institution admin guard
+  // TODO: Auth guard
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Пользователь создан',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Ошибка валидации тела запроса',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Пользователь не авторизован',
+  })
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async create(@Body() dto: CreateUserDto) {
+    return await this.usersService.create(dto);
+  }
+
+  // TODO: Check userId if role is student or teacher
+  // TODO: Auth guard
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Пользователь обновлён',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Пользователь не авторизован',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return await this.usersService.update(userId, dto);
+  }
+
+  // TODO: Admin and institution admin guard (institution admin cannot delete admin)
+  // TODO: Auth guard
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Пользователь удалён',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Пользователь не авторизован',
+  })
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) userId: number) {
+    return await this.usersService.delete(userId);
+  }
 }
